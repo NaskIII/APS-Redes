480f6f8b95a8f9b1289496eae99eb713a65cf9b4
Raphael Nascimento <raphael_nask@hotmail.com>
2019-09-08 23:05:31 -0300

    O server foi criado e autenticação também foi implementada

---
+++ Input.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append('/home/nask/Documentos/Repositórios/APS-Redes/')
+from Server import ServerFTP
+
+def start():
+    def createUser():
+        user = input('Digite seu nome de usuário: ')
+        return user
+    
+    def createPassword():
+        password = int(input('Digite sua senha: '))
+        return password
+    
+    client = {
+        'User': createUser(),
+        'Password': createPassword()
+    }
+
+    def call():
+        ServerFTP.ServerFTP(client, '/home/nask/Documentos/Arquivos/').caller()
+    call()
+start()
