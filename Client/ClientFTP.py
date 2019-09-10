2a77e890cf20d3ef27477d8762cb918819c6b8e0
Raphael Nascimento <raphael_nask@hotmail.com>
2019-09-10 02:00:23 -0300

    A aplicação consegue mandar e realizar o download, porem só na mesma máquina

--- Client/ClientFTP.py
+++ Client/ClientFTP.py
@@ -1,47 +1,65 @@
 from ftplib import FTP
 import sys
 import os.path
+import os
+import shutil
 
 
 class ClientFTP(object):
-     def __init__(self, client):
+    def __init__(self, client):
         self.user = client['User']
         self.password = client['Password']
 
-     def connect(self):
+    def connect(self):
         ftp = FTP('')
         ftp.connect('localhost', 1026)
         try:
-         ftp.login(user=self.user, passwd=self.password, acct='')
-         dir = os.path.expanduser('~\\OneDrive\\Documentos\\')
-         ftp.cwd(dir)
-         return ftp
+            ftp.login(user=self.user, passwd=self.password, acct='')
+            #dir = os.path.expanduser('~\\OneDrive\\Documentos\\')
+            return ftp
         except:
-           print('Usuário e/ou senha incorretos.')
-           sys.exit()
+            print('Usuário e/ou senha incorretos.\n')
+            sys.exit()
 
-     def download(self, ftp):
+    def download(self, ftp):
+        caminho = input('Informe o arquivo ')
+        try:
+            arquivo = open(caminho, 'wb')
+            ftp.retrbinary('RETR ' + caminho, arquivo.write, 1024)
+            arquivo.close()
+        except:
+            print('Caminho inválido!\n')
+            self.escolha()
+
+    def upload(self, ftp):
         caminho = input('Digite o caminho absoluto do arquivo: ')
-        arquivo = open(caminho, 'wb')
-        ftp.retrbinary('RETR ' + caminho, arquivo.write, 1024)
-        ftp.quit()
-        arquivo.close()
+        arq = input('Digite o nome do arquivo: ')
+        try:
+         shutil.copy(caminho, '/home/nask/Documentos/Repositórios/APS-Redes/')
+         arquivo = open(arq, 'rb')
+         ftp.storbinary('STOR ' + arq, arquivo)
+         shutil.move('/home/nask/Documentos/Repositórios/APS-Redes/' + arq, caminho)
+        except:
+           print('Caminho inválido!\n')
+           self.escolha()
 
-     def upload(self, ftp):
-        caminho = input('Digite o caminho absoluto do arquivo: ')
-        ftp.storbinary('STOR '+caminho, open(caminho, 'rb'))
-        ftp.quit()
-
-     def listar(self, ftp):
+    def listar(self, ftp):
         ftp.retrlines('LIST')
 
-     def call(self):
+    def escolha(self):
         ftp = self.connect()
-        escolha = int(input('1 - Download \n2 - Upload \n3 - Listar\n \n>>> '))
-        
-        if escolha == 1:
-           self.download(ftp)
-        elif escolha == 2:
-           self.upload(ftp)
-        else:
-           self.listar(ftp)
+
+        escolha = ''
+        while escolha != 'exit':
+            escolha = input('\n1 - Download \n2 - Upload \n3 - Listar\n \n>>> ')
+            self.call(escolha, ftp)
+
+    def call(self, escolha, ftp):
+       if escolha == '1':
+          self.download(ftp)
+       elif escolha == '2':
+            self.upload(ftp)
+       elif escolha == '3':
+            self.listar(ftp)
+       elif escolha == 'exit':
+            ftp.close()
