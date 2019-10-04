5e8759da72c1d51211316072df8b83bb42cd43e3
NaskIII <raphael_nask@hotmail.com>
2019-10-04 00:43:15 -0300

    Bugs consertados e testes realizados

--- Client/ClientFTP.py
+++ Client/ClientFTP.py
@@ -1,5 +1,6 @@
 from ftplib import FTP
 import sys
+import platform
 
 
 class ClientFTP(object):
@@ -33,13 +34,26 @@ class ClientFTP(object):
     def upload(self, ftp):
         path = input('Informe o caminho do arquivo: ')
 
+        so = platform.system()
+        nomeArq = ''
+
+        if so == 'Linux':
+            nomeArq = path.split('/')
+            nomeArq = nomeArq[-1]
+        elif so == 'Windows':
+            nomeArq = path.split('\\')
+            nomeArq = nomeArq[-1]
+        else:
+            print('Sistema Operacional não suportado!')
+            sys.exit()
+
         try:
-         archive = open(path, 'rb')
-         ftp.storbinary('STOR ' + path, archive)
+            archive = open(path, 'rb')
+            ftp.storbinary('STOR ' + nomeArq, archive)
 
         except:
-           print('Caminho inválido!\n')
-           self.menu(self.ip)
+            print('Caminho inválido!\n')
+            self.menu(self.ip)
 
     def listar(self, ftp):
         ftp.retrlines('LIST')
@@ -54,12 +68,12 @@ class ClientFTP(object):
             self.call(escolha, ftp)
 
     def call(self, escolha, ftp):
-       if escolha == '1':
-          self.download(ftp)
-       elif escolha == '2':
+        if escolha == '1':
+            self.download(ftp)
+        elif escolha == '2':
             self.upload(ftp)
-       elif escolha == '3':
+        elif escolha == '3':
             self.listar(ftp)
-       elif escolha == '!EXIT':
-           print('\nAté logo!')
-           sys.exit()
+        elif escolha == '!EXIT':
+            print('\nAté logo!')
+            sys.exit()
